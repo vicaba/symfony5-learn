42bcca1bc81fe2a0d7c5638e641a793510a8b7b6
vcaballero-salle <victor.caballero@salle.url.edu>
2020-12-28 17:52:39 +0100

    Console command created

--- app/film/Command/GetFilmByIdCommand.php
+++ app/film/Command/GetFilmByIdCommand.php
@@ -13,5 +13,36 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class GetFilmByIdCommand extends Command
 {
+    private const INPUT_FILM_ID = "filmId";
 
+    private GetFilmByIdUseCase $getFilmByIdUseCase;
+    private FilmArraySerializer $filmArraySerializer;
+
+    public function __construct(GetFilmByIdUseCase $getFilmByIdUseCase, FilmArraySerializer $filmArraySerializer)
+    {
+        $this->getFilmByIdUseCase = $getFilmByIdUseCase;
+        $this->filmArraySerializer = $filmArraySerializer;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName("getfilmbyid")
+            ->addArgument(self::INPUT_FILM_ID, InputArgument::REQUIRED, "Film id");
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filmId = $input->getArgument(self::INPUT_FILM_ID);
+
+        $film = $this->getFilmByIdUseCase->__invoke(new GetFilmByIdRequest($filmId));
+
+        $filmAsJson = json_encode($this->filmArraySerializer->__invoke($film));
+
+        $output->writeln($filmAsJson);
+
+        return Command::SUCCESS;
+    }
 }
