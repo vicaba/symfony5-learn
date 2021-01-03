b0fbb2cba8fc006f9c0fabf6b94c81aa82063e94
vcaballero-salle <victor.caballero@salle.url.edu>
2021-01-03 18:11:09 +0100

    product getters created

--- src/Film/Domain/Entity/Product.php
+++ src/Film/Domain/Entity/Product.php
@@ -23,15 +23,36 @@ class Product
     /**
      * @ORM\Column(type="string", length=100)
      */
-    private $name;
+    private string $name;
 
     /**
      * @ORM\Column(type="decimal", scale=2)
      */
-    private $price;
+    private float $price;
 
     /**
      * @ORM\Column(type="text")
      */
-    private $description;
+    private string $description;
+
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function price(): float
+    {
+        return $this->price;
+    }
+
+    public function description(): string
+    {
+        return $this->description;
+    }
 }
